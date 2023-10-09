9b178b637b2aa65b32885bfa5a90a5473c5d3a9d
joan <joan.iragorri@gmail.com>
2023-10-09 15:23:12 -0500

    se realiza cambios en el power bi

--- codificacion/conexion_sql_server_a_RStudio.R
+++ codificacion/conexion_sql_server_a_RStudio.R
@@ -5,3 +5,7 @@ con <- dbConnect(odbc::odbc(), driver = "ODBC Driver 17 for SQL Server",
 query <- "SELECT * FROM Bases_Practica.dbo.vw_informe_ventas_consolidado"
 df <- dbGetQuery(con, query)
 print(df)
+library(ggplot2)
+
+ggplot(data = na.omit(df), mapping = aes(x = df$Estado_civil_vendedor, y = df$Salario_vendedor)) +
+  geom_boxplot()
